8ee3e4f6eb474d9a2607b943d7a43cbae2be0977
willianbs <willianbs@gmail.com>
2019-06-02 03:03:26 -0300

    creating and removing groups

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,8 +6,9 @@ import Users from './pages/Users';
 import UserDetail from './pages/Users/Detail';
 import NewUser from './pages/Users/NewUser';
 import EditUser from './pages/Users/EditUser';
-// import Groups from './pages/Users';
-// import GroupDetail from './pages/Groups/Detail';
+import Groups from './pages/Groups';
+import GroupDetail from './pages/Groups/Detail';
+import NewGroup from './pages/Groups/NewGroup';
 
 const Routes = () => (
   <BrowserRouter>
@@ -18,8 +19,9 @@ const Routes = () => (
       <Route path="/user/new" exact component={NewUser} />
       <Route path="/user/:id/edit" component={EditUser} />
       <Route path="/user/:id" component={UserDetail} />
-      {/* <Route path="/groups/" component={Groups} />
-      <Route path="/group/:id" component={GroupDetail} /> */}
+      <Route path="/groups/" component={Groups} />
+      <Route path="/group/new" exact component={NewGroup} />
+      <Route path="/group/:id" component={GroupDetail} />
     </Switch>
   </BrowserRouter>
 );
